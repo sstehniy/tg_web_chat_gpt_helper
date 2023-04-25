9340012c25a86af6d86e8f49a895c12f770d9e8f
Stanislav Stehniy <sstehniy@gmail.com>
2023-04-25 19:32:04 +0200

    minor change to promt configs

--- src/propmpts_config.ts
+++ src/propmpts_config.ts
@@ -5,13 +5,13 @@ In this task, you'll be given a chat log between two people: 'ME' and 'OTHER'. E
 ME: MESSAGE
 or
 OTHER: MESSAGE
-The final message starts with 'SELECTED' and is part of the chat log. Based on the context from the 'ME' and 'OTHER' messages, craft a short, appropriate response to the 'SELECTED' message as if you were participating in the conversation. Please provide only the message content, without the 'ME:' prefix. If unsure of a response, still reply to the 'SELECTED' message. Please output the response in the primary language of the conversation.
+The final message starts with 'SELECTED' and is part of the chat log. Based on the context from the 'ME' and 'OTHER' messages, craft a short, appropriate one sentence response to the 'SELECTED' message as if you were participating in the conversation. Please provide only the message content, without the 'ME:' prefix. If unsure of a response, still reply to the 'SELECTED' message. Please output the response in the primary language of the conversation.
 
 Consider these guidelines:
   
 Writing style: Conversational
 Tone: Neutral
-Maximum length: 50 words
+Maximum length: 30 words
 
 Ensure these guidelines are followed when creating your response.  
 `,
@@ -26,7 +26,7 @@ Consider these guidelines:
 
 Writing style: {{OUTPUT_WRITING_STYLE}}
 Tone: {{OUTPUT_TONE}}
-Maximum length: maximum 50 words
+Maximum length: maximum 30 words
 
 Ensure these guidelines are followed when creating your response.
 `
